1a2f6d5e6ec1b358fecf5972631ee38eb6c72279
Michael Klein <mklein1997@icloud.com>
2020-12-13 15:08:25 -0500

    Part 1 Written
    Load data into tbody element, pending filter function.

--- UFO Level 1/static/js/app.js
+++ UFO Level 1/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data);
+
+data.forEach(function(ufosighting) {
+    console.log(ufosighting);
+});
+
+data.forEach(function(ufosighting) {
+    console.log(ufosighting); 
+    var row = tbody.append("tr");
+
+    Object.entries(ufosighting).forEach(function([key, value]) {
+        console.log(key, value);
+    });
+});
+
+data.forEach(function(ufosighting) {
+    console.log(ufosighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufosighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
